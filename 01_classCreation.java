41014433528ba8139109ea6408bbd92eff03ff69
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-25 22:08:28 +0530

    changed name

---
+++ 01_classCreation.java
@@ -0,0 +1,7 @@
+public class 01_classCreation {
+    public static void main(String[] args) {
+        System.out.println("This is how a class is created ");
+    }
+}
+// class classCreation is a class which basically is a blueprint thats consists of various value 
+// attributes etc.
