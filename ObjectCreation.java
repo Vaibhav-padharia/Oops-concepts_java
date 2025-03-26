01ed56bc5b157c1bb2dbd71dc66d836d2b3474e6
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-26 22:21:34 +0530

    creation of object

---
+++ ObjectCreation.java
@@ -0,0 +1,28 @@
+public class ObjectCreation {
+
+public void objectMethod() {
+
+    System.out.println("This is a object");
+    
+}
+}
+   class Test
+{
+    public static void main(String[] args) {
+        {
+            ObjectCreation obj1= new   ObjectCreation();
+
+              obj1.objectMethod();
+
+            
+        }
+    }
+}   
+
+
+
+
+
+
+
+
