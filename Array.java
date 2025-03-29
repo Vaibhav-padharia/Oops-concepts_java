50518a60f0953080f1105a1ceb606b126641f778
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-29 23:31:34 +0530

    another way to define array

--- Array.java
+++ Array.java
@@ -15,3 +15,26 @@ public class Array {
 
     }
 }
+
+
+
+
+public class Array {
+    public static void main(String[] args) {
+        // int[] Marks= new int[3];      // this is how we define array of spercific datatype 
+        //  Marks[0]= 89;//phy             // in java indexing starts from 0 
+        //  Marks[1]= 89;//math
+        //  Marks[2]= 89;//eng
+        // //  System.out.println(Marks[2]);              //prints the value 89
+        // //  System.out.println(Marks[1]);
+        // //  System.out.println(Marks[0]);
+           
+
+        int Marks[ ]= {89,89,89};     // we can even define array in this way   type arrayname[]={values};
+        for(int i=0;i<3;i++)
+        {
+             System.out.println( Marks[i]);     // we can even use a loop as well
+        }
+
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,15 +1,17 @@
-        int[] Marks= new int[3];      // this is how we define array of spercific datatype 
-         Marks[0]= 89;//phy             // in java indexing starts from 0 
-         Marks[1]= 89;//math
-         Marks[2]= 89;//eng
-        //  System.out.println(Marks[2]);              //prints the value 89
-        //  System.out.println(Marks[1]);
-        //  System.out.println(Marks[0]);
+public class Array {
+//     public static void main(String[] args) {
+//         int[] Marks= new int[3];      // this is how we define array of spercific datatype 
+//          Marks[0]= 89;//phy             // in java indexing starts from 0 
+//          Marks[1]= 89;//math
+//          Marks[2]= 89;//eng
+//         //  System.out.println(Marks[2]);              //prints the value 89
+//         //  System.out.println(Marks[1]);
+//         //  System.out.println(Marks[0]);
            
-        for(int i=0;i<3;i++)
-        {
-             System.out.println( Marks(i));
-        }
+//         for(int i=0;i<3;i++)
+//         {
+//              System.out.println( Marks[i]);     // we can even use a loop as well
+//         }
 
-    }
-}
+//     }
+// }
