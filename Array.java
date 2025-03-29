51e44bc2a11ece4b9d2220105213239f0f293fa5
Vaibhav-padharia <jaypadharia19@gmail.com>
2025-03-29 23:04:19 +0530

    array

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] Marks= new int[3];      // this is how we define array of spercific datatype 
+         Marks[0]= 89;//phy             // in java indexing starts from 0 
+         Marks[1]= 89;//math
+         Marks[2]= 89;//eng
+         System.out.println(Marks[2]);
+         System.out.println(Marks[1]);
+         System.out.println(Marks[0]);
+
+
+    }
+}
